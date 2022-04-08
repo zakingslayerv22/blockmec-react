032a63b5bfb88432e92cd48ae6a7413bda712836
talk2tim <timothyazubuikeh@gmail.com>
2022-04-08 20:34:51 +0800

    test

--- src/components/Team.js
+++ src/components/Team.js
@@ -78,7 +78,7 @@ const [teams, setTeams] = useState([
                     <div className="team-member text-center">
                         <div className="team-img">
                             {/* <img src="assets/images/team3.jpg" alt="" className="radius-image" /> */}
-                            <img src="assets/images/blockmec-images/team/team1.png" alt="team2" className="radius-image" />
+                            <img src="assets/images/blockmec-images/team/team2.png" alt="team2" className="radius-image" />
                             <div className="overlay-team">
                                 <div className="team-details text-center">
                                     <div className="socials mt-20">
